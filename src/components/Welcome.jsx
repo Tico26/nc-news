09eb791703be04210968b3ebb5d2473e777119b8
Bruno Jauad <brunojauad26@icloud.com>
2024-12-29 20:21:57 +0000

    small sign in section

--- src/components/Welcome.jsx
+++ src/components/Welcome.jsx
@@ -7,14 +7,14 @@ export const Welcome = () => {
       <div>
         <h1>Northcoders News Network</h1>
         <h4>News to entertain</h4>
-      </div>
-      <div className="welcome-buttons-container">
-        <Link to="/articles" className="button">
-          <p>All Articles</p>
-        </Link>
-        <Link to="/topics" className="button">
-          <p>All Topics</p>
-        </Link>
+        <div className="welcome-buttons-container">
+          <Link to="/articles" className="button">
+            <p>All Articles</p>
+          </Link>
+          <Link to="/topics" className="button">
+            <p>All Topics</p>
+          </Link>
+        </div>
       </div>
     </section>
   );
